b1559bcf189409e6c0862709c011da0d9d67fb9c
zhefanlin <zhefan.lin@gmail.com>
2016-01-16 12:42:28 -0500

    First commit

---
+++ src/main/java/com/evergage/exercise/count_world/WordCount.java
@@ -0,0 +1,13 @@
+package com.evergage.exercise.count_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class WordCount 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
